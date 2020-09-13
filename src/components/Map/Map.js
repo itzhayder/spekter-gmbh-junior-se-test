71845c7c06e73b4728821952af34c2164cde8085
itzhayder <mdhayder.ali@northsouth.edu>
2020-09-13 20:45:49 +0600

    implement Geolocation API

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { GoogleMap, useLoadScript, Marker,  } from '@react-google-maps/api';
 import Modal from '../Modal/Modal';
 
@@ -7,20 +7,24 @@ const mapStyles = {
   width: "100vw"
 };
 
-const myPosition = { 
-  lat: 23.81975079, 
-  lng: 90.43176054
-}; // Bashundhara R/A, Block F
+const defaultPosition = { 
+  lat: 23.6850, 
+  lng: 90.3563 
+}; // Bangladesh
 
-// give your Google MAP API Key here
+// give your Google MAP API Key here or set it into the environment variable
 const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API;
 
 const Map = () => {
   // eslint-disable-next-line
   const [ map, setMyMap ] = useState(null);
-  // eslint-disable-next-line
-  const [ center, setCenter ] = useState(myPosition);
   const [ open, setOpen ] = useState(false);
+  const [ state, setState ] = useState({ 
+    position: defaultPosition, 
+    center: defaultPosition, 
+    showMarker: false 
+  });
+  
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: GOOGLE_MAPS_API_KEY
@@ -34,15 +38,32 @@ const Map = () => {
     setOpen(false);
   };
 
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((currPosition) => {
+        const clientCurrentPosition = {
+          lat: currPosition.coords.latitude,
+          lng: currPosition.coords.longitude
+        };
+
+        setState(prevState => ({ ...prevState, position: clientCurrentPosition, center: clientCurrentPosition, showMarker: true }));
+      }, (err) => {
+        alert('Your location is blocked. Please give the location access and reload');
+      });
+    } else {
+      alert('To get the full experience, please enable your location');
+    }
+  }, []);
+
   const renderMap = () => (
     <GoogleMap
       mapContainerStyle={mapStyles}
-      zoom={13}
-      center={center}
+      zoom={10}
+      center={state.center}
       onLoad={map => setMyMap(map)}
     >
-      <Marker position={myPosition} onClick={handleClickOpen} />
-      { open ? <Modal coordinates={myPosition} open={open} handleClose={handleClose} /> : null }
+      { state.showMarker ? <Marker position={state.position} onClick={handleClickOpen} /> : null }
+      { open ? <Modal coordinates={state.position} open={open} handleClose={handleClose} /> : null }
     </GoogleMap>
   );
 
